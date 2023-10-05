1059ca7151738eafefb01ad75fcf1ef1c4b908ad
Matheus Felix <139878742+MFMarinho@users.noreply.github.com>
2023-10-05 14:20:28 +0000

    exercicios

---
+++ exercicio05-10.ts
@@ -0,0 +1,53 @@
+import readline from "readline-sync";
+
+// EXERCICIO 01
+
+// class Pessoa {
+//     nome: string;
+//     idade: number;
+//     email: string;
+//     constructor(nome: string, idade: number, email: string) {
+//         this.nome = nome;
+//         this.idade = idade;
+//         this.email = email;   
+//     }
+// }
+
+// let matheus = new Pessoa ("Matheus", 27, "email@email.com");
+// console.log(matheus);
+
+// EXERCICIO 02
+
+// class Aluno extends Pessoa {
+//     matricula: number;
+//     constructor (matricula: number){
+//         super();
+//         this.matricula = matricula;
+//     }
+// }
+
+// let lucas = new Aluno();
+
+// EXERCICIO 03
+
+// class Retangulo {
+//     largura: number;
+//     altura: number;
+//     constructor(largura: number, altura: number){
+//         this.largura = largura;
+//         this.altura = altura;
+//     }
+
+//     area() {
+//         return this.largura * this.altura;
+//     }
+//     perimetro() {
+//         return (this.largura*2) + (this.altura*2);
+//     }
+
+// }
+
+// let teste = new Retangulo (1,2);
+// console.log(teste);
+
+
